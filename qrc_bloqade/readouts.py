a72245bcdb2d24424d35bbabe365e615c98929de
Priyanshi <142280579+Cass490@users.noreply.github.com>
2025-02-21 15:34:26 +0530

    fixed a little

---
+++ qrc_bloqade/readouts.py
@@ -0,0 +1,34 @@
+import bloqade
+from bloqade import spin_operators
+import numpy as np
+
+
+class ZReadout:
+    def __init__(self, n_sites, readout_indices=None):
+        self.n_sites = n_sites
+        if readout_indices is not None:
+           self.readout_indices = readout_indices
+        else:
+           self.readout_indices = list(range(self.n_sites))
+    def generate_readouts(self, n_sites):
+       #Generates single-qubit Z measurements as readout operators.
+        readouts = []
+        for i in range(n_sites):
+            readouts.append(bloqade.spin_operators.spin_z(i))
+        return readouts 
+
+    def measure(self, evolved_results, readout_indices=None):
+       # Measure the expectation value of each spin in the Z-basis.
+
+        # Use provided indices or default to all
+        if readout_indices is not None:
+            self.readout_indices = readout_indices #update with the new indices
+
+        features = []
+        for result in evolved_results:
+            expectation_values = []
+            for i in self.readout_indices:  # Only measure specified qubits
+                expectation_values.append(bloqade.expectation(result.states[-1], spin_operators.sigma_z(i)).real)
+            features.append(expectation_values)
+
+        return np.array(features)
