80f6c106907ff8af3dd5cbc7171c6204b2154103
Priyanshi <142280579+Cass490@users.noreply.github.com>
2025-02-21 16:52:04 +0530

    new file

---
+++ qrc_bloqade/solver.py
@@ -0,0 +1,23 @@
+# qrc_bloqade/solvers.py
+import bloqade
+
+class BloqadeSolver:
+    """
+    A solver that uses the bloqade library to perform the simulation.
+    """
+
+    def simulate(self, init_state, hamiltonian, duration, steps):
+        """
+        Simulates the time evolution of a quantum system using Bloqade.
+
+        Args:
+            init_state: The initial state of the system.
+            hamiltonian: The Hamiltonian governing the system's evolution.
+            duration: The total simulation time.
+            steps: The number of time steps.
+
+        Returns:
+            A bloqade.Result object containing the simulation results.
+        """
+
+        return bloqade.simulate(init_state, hamiltonian=hamiltonian, duration=duration, steps=steps)
---
+++ tst.py
@@ -0,0 +1,3 @@
+from . import qrc_bloqade.encodings
+
+#i want to kill myself
